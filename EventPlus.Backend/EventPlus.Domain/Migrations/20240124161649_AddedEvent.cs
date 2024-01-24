e56472b57c163df5b68ce544e69a92a3f5f2d5b0
KulchytskiyDmytro <70943031+KulchytskiyDmytro@users.noreply.github.com>
2024-01-24 18:01:58 +0200

    EventPlus migrations

---
+++ EventPlus.Backend/EventPlus.Domain/Migrations/20240124161649_AddedEvent.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventPlus.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Priority = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<long>(type: "bigint", nullable: false),
+                    Deleted = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CommandId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Event_AppUsers_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "AppUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Event_Command_CommandId",
+                        column: x => x.CommandId,
+                        principalTable: "Command",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Event_CommandId",
+                table: "Event",
+                column: "CommandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Event_CreatorId",
+                table: "Event",
+                column: "CreatorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Event");
+        }
+    }
+}
