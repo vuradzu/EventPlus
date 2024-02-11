481c1e98766800d08601288a4287fcfb516ceb9f
tweekerr <gektweeker@gmail.com>
2024-02-12 00:24:57 +0200

    t-6: Updatable events fix

---
+++ EventPlus.Backend/EventPlus.Domain/Migrations/20240211221151_EventUpdatable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EventPlus.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventUpdatable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "Event",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "UpdatedBy",
+                table: "Event",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "Event");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "Event");
+        }
+    }
+}
