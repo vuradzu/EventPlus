1189ca0fae88066f0702e64ad0273d71e1afcce8
KulchytskiyDmytro <70943031+KulchytskiyDmytro@users.noreply.github.com>
2024-02-26 22:27:26 +0200

    t-12: Get One Assignment + Controller for Delete And Get One

--- EventPlus.Backend/EventPlus.Api/Controllers/AssignmentController.cs
+++ EventPlus.Backend/EventPlus.Api/Controllers/AssignmentController.cs
@@ -23,4 +23,25 @@ public class AssignmentController : Controller
         return await handler.Handle(request, ct);
     }
 
+    
+    /// <summary>
+    /// Delete Assignment
+    /// </summary>
+    [HttpDelete("{id}")]
+    public async Task Delete([FromRoute] long id,
+        [FromServices] DeleteAssignmentHandler handler, CancellationToken ct)
+    {
+        await handler.Handle(new DeleteAssignmentRequest(){Id = id}, ct);
+    }
+    
+    /// <summary>
+    /// Get One Assignment by id
+    /// </summary>
+    [HttpGet("{id}")]
+    public async Task<AssignmentsModel> GetOne([FromRoute] long id,
+        [FromServices] GetOneAssignmentHandler handler, CancellationToken ct)
+    {
+        return await handler.Handle(new GetOneAssignmentRequest(){Id = id}, ct);
+    }
+    
 }
---
+++ EventPlus.Backend/EventPlus.Application/Minis/Assignments/Get/GetOne/GetOneAssignmentRequest.cs
@@ -0,0 +1,19 @@
+﻿using EventPlus.Application.Minis.Assignments.Models;
+using EventPlus.Application.Minis.Base;
+using FluentValidation;
+
+namespace EventPlus.Application.Minis.Assignments.Get.GetOne;
+
+public class GetOneAssignmentRequest : IMinisRequest<AssignmentsModel>
+{
+    public required long Id { get; set; }
+}
+
+internal sealed class GetOneAssignmentValidator : AbstractValidator<GetOneAssignmentRequest>
+{
+    public GetOneAssignmentValidator()
+    {
+        RuleFor(i => i.Id)
+            .NotEmpty().NotNull().WithMessage("Invalid Id");
+    }
+}
