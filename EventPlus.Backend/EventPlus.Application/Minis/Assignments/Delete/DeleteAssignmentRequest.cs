0830f8f1069bb913322af36bbadeea9617a86967
KulchytskiyDmytro <70943031+KulchytskiyDmytro@users.noreply.github.com>
2024-02-26 22:25:43 +0200

    t-12: Delete Assignment

---
+++ EventPlus.Backend/EventPlus.Application/Minis/Assignments/Delete/DeleteAssignmentRequest.cs
@@ -0,0 +1,18 @@
+﻿using EventPlus.Application.Minis.Base;
+using FluentValidation;
+
+namespace EventPlus.Application.Minis.Assignments.Delete;
+
+public class DeleteAssignmentRequest : IMinisRequest
+{
+    public required long Id { get; set; }
+}
+
+internal sealed class DeleteAssignmentValidator : AbstractValidator<DeleteAssignmentRequest>
+{
+    public DeleteAssignmentValidator()
+    {
+        RuleFor(i => i.Id)
+            .NotEmpty().NotNull().WithMessage("Invalid Id");
+    }
+}
