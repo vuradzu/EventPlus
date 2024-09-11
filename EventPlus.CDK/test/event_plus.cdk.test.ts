2ed0c2912d342ba639f1e53d80be5f64d055c4e8
tweekerr <gektweeker@gmail.com>
2024-09-11 17:31:28 +0300

    CDK Initialization

---
+++ EventPlus.CDK/test/event_plus.cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventPlusCdk from '../lib/event_plus.cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/event_plus.cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventPlusCdk.EventPlusCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
